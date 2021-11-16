8b429d540e214d246c85fa65079f70039285d20a
Karthikeya Chenna <84320738+karthikeyachenna@users.noreply.github.com>
2021-11-16 11:03:23 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,61 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color="blue";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e)
+{
+    
+    mouse_x=e.clientX-canvas.offsetLeft;
+    mouse_y=e.clientY-canvas.offsetTop;
+    console.log ("X = " + mouse_x + " ,Y = " + mouse_y); circle(mouse_x , mouse_y);
+}
+function circle(mouse_x , mouse_y) { ctx.beginPath(); 
+    ctx.strokeStyle = color;
+    
+    ctx.lineWidth = 2; 
+    ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+     ctx.stroke(); 
+    }
+    canvas=document.getElementById("myCanvas");
+    canvas=document.getElementById("myCanvas");
+    ctx=canvas.getContext("2d");
+    color="yellow";
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(240,240,40,0,2*Math.PI);
+    ctx.stroke();
+    canvas.addEventListener("mousedown",my_mousedown);
+    canvas=document.getElementById("myCanvas");
+    canvas=document.getElementById("myCanvas");
+    ctx=canvas.getContext("2d");
+    color="black";
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(290,200,40,0,2*Math.PI);
+    ctx.stroke();
+    canvas.addEventListener("mousedown",my_mousedown);
+    canvas=document.getElementById("myCanvas");
+    ctx=canvas.getContext("2d");
+    color="green";
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(340,240,40,0,2*Math.PI);
+    ctx.stroke();
+    canvas.addEventListener("mousedown",my_mousedown);
+    canvas=document.getElementById("myCanvas");
+    ctx=canvas.getContext("2d");
+    color="red";
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(380,200,40,0,2*Math.PI);
+    ctx.stroke();
+    canvas.addEventListener("mousedown",my_mousedown);
